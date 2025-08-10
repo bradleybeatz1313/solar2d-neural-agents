5c1591284196421848bbfbcdd9c6943e5b8682a5
Brad Barroso <bradley.s.barroso@gmail.com>
2025-08-10 10:45:00 -0700

    add network topology constants to config

--- config.lua
+++ config.lua
@@ -17,3 +17,8 @@ Config.MUTATION_RATE     = 0.12   -- probability of mutating each weight
 Config.MUTATION_STRENGTH = 0.25   -- gaussian noise standard deviation
 Config.ELITISM_COUNT     = 4      -- top agents copied unchanged each generation
 Config.CROSSOVER_PROB    = 0.6    -- probability of crossover vs. cloning
+
+-- Network topology
+Config.INPUT_SIZE  = 8   -- sensor inputs per agent
+Config.HIDDEN_SIZES = {16, 12}  -- hidden layer sizes
+Config.OUTPUT_SIZE = 4   -- action outputs
